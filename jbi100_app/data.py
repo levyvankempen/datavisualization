af459b598c4d9a373f6f0d99f8aeea6cb8311210
MaxMaas <113433909+MaxMaas@users.noreply.github.com>
2023-05-15 11:43:21 +0200

    Data columns to match the report

--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -18,21 +18,20 @@ def get_data():
 
 def get_relevant_features():
     # define features
-    useful_cols_GK = ['gk_clean_sheets', 'gk_goals_against', 'gk_goals_against_per90', 'gk_shots_on_target_against',
-                      'gk_saves', 'gk_save_pct', 'gk_pens_saved', 'gk_pens_att_against', 'gk_crosses_stopped_pct',
-                      'gk_aerials_won_pct']
+    useful_cols_GK = ['player', 'position', 'club', 'team', 'age', 'gk_clean_sheets_pct', 'gk_goals_against', 'gk_goals_against_per90', 'gk_passes_completed_launched',
+                      'gk_saves', 'gk_save_pct', 'gk_pens_saved_pct', 'gk_crosses_stopped_pct']
 
-    useful_cols_DF = ['player', 'position', 'club', 'team', 'age', 'minutes_90s', 'tackles_won', 'interceptions',
-                      'clearances', 'aerials_won_pct', 'blocks', 'cards_yellow', 'cards_red', 'fouls', 'own_goals',
+    useful_cols_DF = ['player', 'position', 'club', 'team', 'age', 'tackles_won', 'interceptions',
+                      'clearances', 'aerials_won_pct', 'blocks', 'passes_completed', 'tackles_def_3rd', 'fouls', 'tackles_interceptions',
                       'ball_recoveries']
 
-    useful_cols_MF = ['player', 'position', 'club', 'team', 'age', 'games_starts', 'minutes_90s', 'goals_assists_per90',
-                      'xg_xg_assist_per90', 'tackles_won', 'interceptions', 'passes_pct', 'gca_passes_live',
-                      'gca_passes_dead']
+    useful_cols_MF = ['player', 'position', 'club', 'team', 'age', 'goals_assists_per90',
+                      'xg_xg_assist_per90', 'tackles_won', 'interceptions', 'passes_pct', 'touches_mid_3rd',
+                      'touches']
 
     useful_col_FW = ['player', 'position', 'club', 'team', 'age', 'goals', 'assists', 'shots_per90',
-                     'shots_on_target_pct', 'xg', 'npxg', 'xg_per90', 'xg_assist', 'xg_assist_per90',
-                     'dribbles_completed_pct']
+                     'shots_on_target_pct', 'aerials_won_pct', 'gca_per90', 'xg', 'xg_per90', 'xg_assist', 'xg_assist_per90',
+                     'shots','pens_made', 'dribbles_completed_pct']
 
     return useful_cols_GK, useful_cols_DF, useful_cols_MF, useful_col_FW
 
